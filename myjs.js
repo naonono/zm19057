23132324feea988f485f73b98782d359088aeb71
naonono <zm19057@josai.ac.jp>
2020-10-13 02:18:42 +0000

    message

--- myjs.js
+++ myjs.js
@@ -1,2 +1,9 @@
-alert('Hello, NAOYA');
+alert('Hello, NAOYA!');
+alert(1+1);
+alert(Math.sprt(1+1));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
 

77c60afcce876f0a016956da2f8e79d63aac781f
naonono <zm19057@josai.ac.jp>
2020-11-10 02:20:24 +0000

    message

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
-function sayhello(){alert('Hello,' + document.getElementByld('who').innerHTML + '!');}
-function taro(){document.getElementByld('who').innerHTML='太郎';}
-function hanako(){document.getElementByld('who').innerHTML='花子';}
+function sayhello68(){alert('Hello,' + document.getElementByld('who68').innerHTML + '!');}
+function taro68(){document.getElementByld('who68').innerHTML='太郎';}
+function hanako68(){document.getElementByld('who68').innerHTML='花子';}
--- ex6-5.js
+++ ex6-5.js
@@ -1,5 +1,7 @@
-var who = 'world';function sayhello65(){alert('Hello,'+ who65 +'!')}
+var who65 = 'world';function sayhello65(){alert('Hello,'+ who65 +'!')}
 function taro65(){who65 ='太郎';}
 function hanako65(){who65 ='花子';}
 
-function gakita(){alert(    );}
+var who66 = 'world';function sayhello66(){alert('gakita,'+ who66 +'!')}
+function taro66(){who66 ='太郎が来た';}
+function hanako66(){who66 ='花子が来た';}

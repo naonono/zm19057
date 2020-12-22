b184b6022361ba67996e49ca260efd48959b52a4
naonono <zm19057@josai.ac.jp>
2020-12-22 02:46:52 +0000

    message

--- ex06-10.js
+++ ex06-10.js
@@ -1,3 +1,5 @@
-function imgchange1() {document.getElementById('logo').src= './image/abm00036792.png';}
+function imgchange1(){document.getElementById('logo').src= './image/abm00036792.png';}
 
 function show_innerHTML(){var result1 =document.getElementById('txt1').innerHTML;alert(result1);}
+
+function show_innerHTML(){var result1 =document.getElementById('logo').innerHTML;alert(result1);}
